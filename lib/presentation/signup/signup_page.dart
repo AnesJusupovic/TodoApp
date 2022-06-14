9b2a7eda306978cf989c309d6ed67ea19404ae44
AnesJusupovic <jusanes04@gmail.com>
2022-06-15 00:45:37 +0200

    first commit

---
+++ lib/presentation/signup/signup_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:todo/presentation/signup/widgets/signup_form.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const SignUpForm(),
+    );
+  }
+}
---
+++ lib/presentation/signup/widgets/signin_register_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SignInRegisterButton extends StatelessWidget {
+  final String buttonText;
+  final Function callback;
+  const SignInRegisterButton(
+      {Key? key, required this.buttonText, required this.callback})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+
+    return InkResponse(
+      onTap: () => callback(),
+      child: Container(
+        height: 40,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: themeData.colorScheme.secondary,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Text(
+            buttonText,
+            style: themeData.textTheme.headline1!.copyWith(
+              fontSize: 14,
+              color: Colors.blueGrey[800],
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
