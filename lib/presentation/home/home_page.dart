61098c6cba0d5d1557ec8536574de7bef9c3c4d9
AnesJusupovic <jusanes04@gmail.com>
2022-07-02 19:43:47 +0200

    Pushed

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -56,7 +56,7 @@ class HomePage extends StatelessWidget {
           }),
         ],
         child: Scaffold(
-          appBar: AppBar(
+          /*appBar: AppBar(
             leading: IconButton(
                 onPressed: () {
                   BlocProvider.of<AuthBloc>(context).add(SignOutPressdEvent());
@@ -64,9 +64,10 @@ class HomePage extends StatelessWidget {
                 icon: Icon(Icons.exit_to_app)),
             title: const Text('Todo'),
           ),
+          */
           body: const HomeBody(),
           floatingActionButton: FloatingActionButton(
-            backgroundColor: Theme.of(context).colorScheme.secondary,
+            backgroundColor: Colors.white,
             onPressed: () {
               AutoRouter.of(context).push(TodoDetailRoute(todo: null));
             },
